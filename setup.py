4b66b1ea052bf8ec06458dfaffa913c0690af412
Brian Cappello <briancappello@gmail.com>
2015-12-08 09:36:05 -0500

    depend on forked grequests because upstream is unmaintained

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@ from setuptools import setup, find_packages
 
 NAME = 'pytradelib'
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
 INSTALL_REQUIRES = ([
     'pandas',
-    'pandas-datareader',
-    'tables',
     'requests',
-    'grequests',
+    'grequests==0.3.0',
     'pytz',
+    'ta-lib',
 ])
+
+DEPENDENCY_LINKS = [
+    'https://github.com/briancappello/grequests/tarball/master#egg=grequests-0.3.0',
+]
 
 setup(
     name=NAME,
@@ -23,6 +27,7 @@ setup(
     author='Brian Cappello',
     author_email='briancappello@gmail.com',
     install_requires=INSTALL_REQUIRES,
+    dependency_links=DEPENDENCY_LINKS,
     packages=find_packages('.'),
     zip_safe=False,
 )
